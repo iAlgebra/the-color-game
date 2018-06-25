682d9660bd42e0e82c3b5d5d8c9756efe32adfab
Maria Gabriela Barrios Cantera <barrios.cantera@gmail.com>
2018-06-25 12:59:16 +1000

    Generating random colors
    randomColor() was created to obtain one random rgb  color.
    generateRandomColors() was created to populate an array with rgb colors num times.

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,4 @@
-var colors = [
-	"rgb(255, 0, 0)", //red
-	"rgb(255, 255, 0)", //yellow
-	"rgb(0, 255, 0)", //green
-	"rgb(0, 255, 255)", //cyan
-	"rgb(0, 0, 255)", //blue
-	"rgb(255, 0, 255)" //magenta
-];
+var colors = generateRandomColors(6); //returns an array of 6 colors
 
 const BODY_BG_COLOR = "#232323";
 
@@ -19,7 +12,6 @@ $colorDisplay.textContent = pickedColor;
 for (var i = 0; i < $squares.length; i++) {
 	//add initial colors to squares
 	$squares[i].style.backgroundColor = colors[i];
-
 	//add click listeners to squares
 	$squares[i].addEventListener("click", function() {
 		//grab color of clicked square
@@ -46,9 +38,34 @@ function changeColors(winningColor) {
 	}
 }
 
-
 function pickColor() {
 	//generates a random int between 0 and the max index of the colors array
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//make an arr
+	var arr = [];
+	//repeat num times 
+	for(var i = 0; i < num; i++) {
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	//return arr
+	return arr;
+}
+
+function randomColor() {//creates one random color
+	//creating the string for the color
+	var arr = [];
+	var str = "rgb(";
+	var str2 = ")";
+	//repeat three times
+	for(var i = 0; i < 3; i++) {
+		//generates a number from 0 to 255 and push into arr
+		arr.push(Math.floor(Math.random() * 256));
+	}
+	var stringColor = str + arr + str2;
+	return stringColor;
 }
